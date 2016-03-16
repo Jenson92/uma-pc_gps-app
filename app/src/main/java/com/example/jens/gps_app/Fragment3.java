df88d2643f175507b04b826ef6b29046e84f0a1e
Jens <jens_Eckrich@yahoo.de>
2016-03-16 12:08:49 +0100

    Second Test for Gradl Problems

--- app/src/main/java/com/example/jens/gps_app/Fragment3.java
+++ app/src/main/java/com/example/jens/gps_app/Fragment3.java
@@ -1,25 +1,96 @@
 package com.example.jens.gps_app;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by Jens on 10.03.2016.
- */
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Fragment3 extends Fragment {
-    public Fragment3() {
-    }
+
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_3, container, false);
-        return rootView;
+        // inflat and return the layout
+        View v = inflater.inflate(R.layout.fragment_3, container,
+                false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+
+//TODO: Improve the permission Check
+        PackageManager pm = this.getContext().getPackageManager();
+        int hasPerm = pm.checkPermission(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                this.getContext().getPackageName());
+
+        System.out.println("Permission: " + hasPerm);
+
+        if (hasPerm != PackageManager.PERMISSION_GRANTED) {
+
+            // do stuff
+        }
+
+        googleMap.setMyLocationEnabled(true);
+
+
+        googleMap.addMarker(new MarkerOptions()
+                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                .position(new LatLng(49.484014, 8.462512))
+                .title("University of Mannheim")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(49.484014, 8.462512)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
-
-
